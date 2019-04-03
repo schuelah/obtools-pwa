447123017fea084b0eaadad7be539e28222ed529
Kevin Dufendach <Kevin.Dufendach@gmail.com>
2019-04-03 11:46:25 -0400

    create selectable button group

--- src/app/pre-eclampsia-calc/pre-eclampsia-calc.component.ts
+++ src/app/pre-eclampsia-calc/pre-eclampsia-calc.component.ts
@@ -6,11 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pre-eclampsia-calc.component.css']
 })
 export class PreEclampsiaCalcComponent implements OnInit {
-  ga: number;
+
+  dilationScore = 1;
+  effacementScore = 0;
+  stationScore = 0;
+  ga = 40;
+  race = 'other';
+  bmi = 20;
+  prior = false;
+
+  cm = 160;
+  kg = 60;
+
+  dilationOptions = ['CLOSED', '1-2cm', '3-4cm', '&ge;5cm'];
+  effacementOptions = ['0-30%', '40-50%', '60-70%', '&ge;80%'];
+  stationOptions = ['-3', '-2', '-1, 0', '+1, +2'];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  setDilationScore(score: number) {
+    this.dilationScore = score;
   }
 
 }
