1f3cd6514f9ef831f14ebb2660ebb45efb1efe89
Kevin Dufendach <Kevin.Dufendach@gmail.com>
2019-04-10 10:15:35 -0400

    half-baked sticky example

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,15 +1,51 @@
-import {Component, Input} from '@angular/core';
+import {Component, HostListener, Input, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.scss']
 })
-export class CalculatorComponent {
+export class CalculatorComponent implements OnInit, OnDestroy {
   @Input() value = 0;
   @Input() title = 'Calculator';
   @Input() label = 'Result';
 
-  constructor() { }
+  calcHeader: any;
+  stickyOffset: number;
+  isStuck = false;
+
+  constructor() {
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  scrollHandler(event) {
+    console.log('Scroll Event: isStuck: ' + this.isStuck + ', offset: ' + window.pageYOffset);
+
+    if (!this.isStuck && window.pageYOffset > this.stickyOffset) {
+      this.calcHeader.classList.add('sticky');
+      this.isStuck = true;
+      console.log('added sticky');
+    } else if (this.isStuck && window.pageYOffset <= this.stickyOffset) {
+      this.calcHeader.classList.remove('sticky');
+      this.isStuck = false;
+      console.log('removed sticky');
+    }
+  }
+
+  ngOnInit(): void {
+    this.calcHeader = document.getElementById('calcHeader');
+    this.stickyOffset = this.calcHeader.offsetTop;
+
+    console.log(this.stickyOffset);
+  }
+
+  ngOnDestroy(): void {
+
+    // window.removeEventListener('scroll', this.scroll, true)
+  }
+
+  scroll = (): void => {
+    // console.log('Scroll Event inside scroll function')
+  }
 
 }
