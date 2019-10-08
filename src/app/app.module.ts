4c24556f0134de841e93f2c41321b2d0ee61be71
Kevin Dufendach <Kevin.Dufendach@gmail.com>
2019-10-08 10:06:48 -0500

    create dashboard component from schematic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,17 @@ import {AppRoutingModule} from './app-routing/app-routing.module';
 import {PreEclampsiaCalcModule} from './pre-eclampsia-calc/pre-eclampsia-calc.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule } from '@angular/material';
 import {FlexModule} from '@angular/flex-layout';
 import {MaternalIcuCalcModule} from './maternal-icu-calc/maternal-icu-calc.module';
 import {ObesityIolCalcModule} from './obesity-iol-calc/obesity-iol-calc.module';
 import {IolCalcModule} from './iol-calc/iol-calc.module';
+import { ToolDashboardComponent } from './tool-dashboard/tool-dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ToolDashboardComponent,
   ],
   imports: [
     BrowserModule,
@@ -37,6 +39,12 @@ import {IolCalcModule} from './iol-calc/iol-calc.module';
     MatListModule,
 
     FlexModule,
+
+    MatGridListModule,
+
+    MatCardModule,
+
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
