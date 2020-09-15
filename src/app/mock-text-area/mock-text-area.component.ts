2b827b80f3a22722d97f28c394ed3665013a1833
Dufendach, Kevin <Kevin.Dufendach@cchmc.org>
2020-09-15 13:50:05 -0400

    update style sheets

--- src/app/mock-text-area/mock-text-area.component.ts
+++ src/app/mock-text-area/mock-text-area.component.ts
@@ -3,18 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-mock-text-area',
   template: '<div class="mock-textarea" contenteditable="true"><ng-content></ng-content></div>',
-  styles: [
-      `
-      .mock-textarea {
-        background-color: #ffffff;
-        border: 1px solid gray;
-        overflow-x: auto;
-        overflow-y: auto;
-        padding: 8px;
-        resize: both;
-      }
-    `
-  ]
+  styleUrls: [ './mock-text-area.component.scss']
 })
 export class MockTextAreaComponent {
 
